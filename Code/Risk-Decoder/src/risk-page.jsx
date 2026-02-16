09bc89f98a19e7b8fa9cea1c92581467c06c858f
marapcam <mp2181@cam.ac.uk>
2026-02-16 14:28:04 +0000

    now purple and updated quick risk page

--- Code/Risk-Decoder/src/risk-page.jsx
+++ Code/Risk-Decoder/src/risk-page.jsx
@@ -2,17 +2,43 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './styles/main-page.css'
+import './styles/InputPage.css'
 import { Link } from 'react-router-dom';
+
+function handleSubmit() {
+navigate('/main')
+}
 
 function RiskPage() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>AI risk decoder ~ risk page</h1>
-       <nav>
-      <Link to="/main">back to main page</Link>
-      </nav>
+      <h1>risk page ~ company</h1>
+      <div className="card">
+            <div className="card__header">
+                <h2>risk 1</h2>
+            </div>
+      </div>
+      <div className="card">
+            <div className="card__header">
+                <h2>risk 2</h2>
+            </div>
+      </div>
+      <div className="card">
+            <div className="card__header">
+                <h2>risk 3</h2>
+            </div>
+      </div>
+      <div className="card">
+            <div className="card__header">
+                <h2>risk 4</h2>
+            </div>
+      </div>
+      <button className="submit-btn" onClick={handleSubmit}>
+        back to main page
+      </button>
     </>
   )
 }
