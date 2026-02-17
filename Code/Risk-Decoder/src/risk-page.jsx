558a75ff7a45c3a975e0524bfe0d6282d5daeceb
marapcam <mp2181@cam.ac.uk>
2026-02-17 11:30:05 +0000

    fixed back button on risk page

--- Code/Risk-Decoder/src/risk-page.jsx
+++ Code/Risk-Decoder/src/risk-page.jsx
@@ -4,14 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import './styles/main-page.css'
 import './styles/InputPage.css'
-import { Link } from 'react-router-dom';
-
-function handleSubmit() {
-navigate('/main')
-}
+import { useNavigate } from 'react-router-dom'
 
 function RiskPage() {
+  const navigate = useNavigate()
   const [count, setCount] = useState(0)
+
+  function handleBack() {
+    navigate('/main')
+  }
 
   return (
     <>
@@ -36,8 +37,8 @@ function RiskPage() {
                 <h2>risk 4</h2>
             </div>
       </div>
-      <button className="submit-btn" onClick={handleSubmit}>
-        back to main page
+      <button className="submit-btn" onClick={handleBack}>
+        main page
       </button>
     </>
   )
