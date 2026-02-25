031c5836573b4c9a1831601fd94941192a99081c
igor-m <blue.hacker.itm@gmail.com>
2026-02-25 11:56:26 +0000

    added logos and symbos, changed main page

--- Code/Risk-Decoder/src/main-page.jsx
+++ Code/Risk-Decoder/src/main-page.jsx
@@ -1,38 +1,74 @@
 import './App.css'
 import './styles/main-page.css'
-import { useNavigate } from 'react-router-dom';
-import biometric from './assets/biometric.svg'
-import safety from './assets/safety.svg'
-import eco from './assets/eco.svg'
-import ChatGPT from './TMP/chatGPT.svg'
+
+import safety from './Proper_icons/information&safety.svg'
+import malicious from './Proper_icons/malicious.svg'
+import autonomy from './Proper_icons/human_autonomy&integrity.svg'
+import misinformation from './Proper_icons/misinformation.svg'
+import toxicty from './Proper_icons/representation&toxicity.svg'
+import enviroment from './Proper_icons/socioeconomics&enviromental.svg'
+
 import search from './assets/search.svg'
+
+import ChatGPT from './TMP/chatGPT.svg'
+import Claude from './TMP/Claude.svg'
+import DALLE from './TMP/DALLE.svg'
+import Gemini from './TMP/Gemini.svg'
+import Copilot from './TMP/Copilot.svg'
+
+
+import {useState} from "react";
 
 const RiskRecord = ({icon_name,risk}) =>{
     return (
         <li className="risk_record">
             <div>
                 <img className="icon" src={icon_name} alt="risk"/>
-                {risk}
+                <div className="risk_text">
+                    {risk}
+                </div>
+
             </div>
         </li>
     )
 }
 
-const Card = ({service_name,risk_page}) => {
-    let navigate = useNavigate();
-    return (
-        <div className="card" onClick={()=>{navigate(risk_page)}}>
-            <div className="card__header">
-                <img className="logo" src={ChatGPT} alt="risk"/>
-                <h2>{service_name}</h2>
-            </div>
-            <div className="card__body">
-                <ul className="card__risks">
-                    <RiskRecord  icon_name={eco} risk="Enviromental Risk"/>
-                    <RiskRecord  icon_name={biometric} risk="Biometric Risk"/>
-                    <RiskRecord  icon_name={safety} risk="Saftey Risk"/>
-                </ul>
+const Card = ({service_name,risk_page,icon}) => {
+    let  [is_flipped,set_is_flipped]= useState(false);
 
+    function flip(){
+        set_is_flipped(!is_flipped);
+        risk_page;
+    }
+    return (
+        <div className={`full_card ${is_flipped ? "flipped" : ""}`}>
+            <div className="card front" onClick={flip}>
+                <div className="card__header">
+                    <img className="logo" src={icon} alt={service_name+" icon"}/>
+                    <h2>{service_name}</h2>
+                </div>
+                <div className="card__body">
+                    <ul className="card__risks">
+                        <RiskRecord  icon_name={toxicty} risk="Representation & Toxicity Risks"/>
+                        <RiskRecord  icon_name={misinformation} risk="Misinformation Risks"/>
+                        <RiskRecord  icon_name={safety} risk="Information & Safety Risks"/>
+                        <RiskRecord  icon_name={malicious} risk="Malicious Use Risks"/>
+                        <RiskRecord  icon_name={autonomy} risk="Human Autonomy & Integrity Risks"/>
+                        <RiskRecord  icon_name={enviroment} risk="Socioeconomic & Environmental Risks"/>
+
+                    </ul>
+                    <div>Button 1</div>
+                    <div>Button 2</div>
+                </div>
+            </div>
+            <div className="card back" onClick={flip}>
+                <div className="card__header">
+                    <img className="logo" src={icon} alt={service_name+" icon"}/>
+                    <h2>{service_name}</h2>
+                </div>
+                <div className="card__body">
+                    Bad for enviroment and what not
+                </div>
             </div>
         </div>
     )
@@ -59,11 +95,11 @@ function MainPage() {
 
           </div>
           <div className="risk_grid">
-              <Card service_name="ChatGPT" risk_page="/risk"/>
-              <Card service_name="Claude" risk_page="/risk"/>
-              <Card service_name="Gemini" risk_page="/risk"/>
-              <Card service_name="DALLE" risk_page="/risk"/>
-              <Card service_name="Copilot" risk_page="/risk"/>
+              <Card service_name="ChatGPT" risk_page="/risk" icon={ChatGPT}/>
+              <Card service_name="Claude" risk_page="/risk" icon={Claude}/>
+              <Card service_name="Gemini" risk_page="/risk" icon={Gemini}/>
+              <Card service_name="DALLE" risk_page="/risk" icon={DALLE}/>
+              <Card service_name="Copilot" risk_page="/risk" icon={Copilot}/>
           </div>
       </div>
   )
