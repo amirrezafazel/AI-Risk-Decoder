55b25d252786e6d3f540abdb7a2ac72c9450a012
FinleyStirk06 <FinleyStirk06@gmail.com>
2026-03-10 21:54:14 +0000

    populated entire database

--- Code/ai_client/summary_generator.py
+++ Code/ai_client/summary_generator.py
@@ -19,8 +19,8 @@ if __name__ == "__main__":
 
     for company in data:
         documents = data[company]["documents"]
-        # "risks" not in data[company] and
-        if data[company]["id"] < 10:
+
+        if "risks" not in data[company]:
             print(f"Generating risk summary for {company}")
             risks = generate_risk_summary(documents)
             data[company]["risks"] = risks["risks"]
