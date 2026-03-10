997bc9d521d07d567b518478757dd642702033d1
marapcam <mp2181@cam.ac.uk>
2026-03-10 21:06:45 +0000

    updated function

--- Code/Risk-Decoder/src/input_embeddings/inputEmbeddings.py
+++ Code/Risk-Decoder/src/input_embeddings/inputEmbeddings.py
@@ -7,9 +7,11 @@ def calculate_embedding(user_input):
      np.save('my_embedding.npy', model.encode(user_input))
 
 # takes embeddings and returns best n matching with input embeddings 
-def best_match(databaseID, n = 1):
-    risks_text = ...                 # TODO when database is populated
-    risks_embeddings = ...           # TODO when database is populated
+def best_match(service_name, record, user_embedding = 0, n = 1):
+    if not user_embedding:
+        return []
+    risks_text = [r["description"] for r in record["risks"]]
+    risks_embeddings = [r["embedding"] for r in record["risks"]]
     model = SentenceTransformer('./embeddingsModel')
     input_embedding = np.load('my_embedding.npy')
     hits = util.semantic_search(input_embedding, risks_embeddings, top_k=n)
